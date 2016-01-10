56479a3f4f8fe41301d8064c4043358cc816629b
Abelardo Castro <lalo@macbook-air-de-abelardo.local>
2016-01-10 10:50:57 -0600

    Initial Commit

---
+++ mini Reto/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  mini Reto
+//
+//  Created by Abelardo Castro on 10/01/16.
+//  Copyright © 2016 Abelardo Castro. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
